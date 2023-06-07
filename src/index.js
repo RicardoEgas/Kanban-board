f4527619bb7c694ac1c49850b2f03f089d38dbc4
Bhekisile <111288560+Bhekisile@users.noreply.github.com>
2023-06-07 11:32:43 +0200

    Add html

--- src/index.js
+++ src/index.js
@@ -1,15 +1,47 @@
 import './index.css';
-// import renderRecipes from './module/displayFood.js';
 
-fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=c')
-  .then((response) => response.json())
-  .then((data) => {
-    const { meals } = data;
+const dogList = document.getElementById('dogs-list');
 
-    meals.forEach((meal) => {
-      const mealCounter = document.getElementById('mealCounter');
-      const num = Object.values(meals).length;
-      mealCounter.innerHTML = `<p>Meals(${num})</p>`;
-      console.log(meal);
-    });
+function displayDogs() {
+  const li = document.createElement('li');
+
+  li.classList.add('card');
+  const likes = document.createElement('span');
+  likes.innerHTML = `
+      <i class="fa fa-heart-o"></i>
+    `;
+  likes.classList.add('like-dog');
+
+  const commentBtn = document.createElement('button');
+  commentBtn.innerHTML = 'Comments';
+  commentBtn.type = 'submit';
+  commentBtn.classList.add('comment-btn');
+
+  const reservationsBtn = document.createElement('button');
+  reservationsBtn.innerHTML = 'Reservations';
+  reservationsBtn.type = 'submit';
+  reservationsBtn.classList.add('reserve-btn');
+
+  li.append(likes, commentBtn, reservationsBtn);
+  dogList.append(li);
+}
+
+const fetchDogs = async () => {
+  try {
+    const response = await fetch('https://dog.ceo/api/breed/akita/images/random');
+    const data = await response.json();
+    return data.message;
+  } catch (error) {
+    return 'something went wrong';
+  }
+};
+// fetchDogs();
+const images = [];
+const dogs = document.getElementById('dogs-list');
+for (let i = 0; i < 6; i += 1) {
+  displayDogs();
+  fetchDogs().then((data) => {
+    images.push(data);
+    dogs.insertAdjacentHTML('afterbegin', `<li><img class="dogs-img" src="${images[i]}"><img></li>`);
   });
+}
