1f8761202b71c3750abe34e5bee813272ff41c13
ometman <revomet@gmail.com>
2023-06-07 23:06:17 +0000

    Add styling to popup image and form elements

--- src/modules/reserve.js
+++ src/modules/reserve.js
@@ -2,21 +2,22 @@
 const reserveDog = (theimgs) => {
   const imgIndex = Object.keys(theimgs);
   const reserveBtn = document.querySelectorAll('.reserve-btn');
-  const reserveModal = document.querySelector('.reserve-modal');
+  const reserveModal = document.querySelector('.reserve-modal');  
   reserveBtn.forEach((btn, btnIndex) => {
     imgIndex.forEach((img, imgIndex) => {
       btn.addEventListener('click', () => {
-       // reserveModal.className = 'reserve-modal';
+        reserveModal.className = 'reserve-modal';
         reserveModal.style.display = 'flex';
         if (btnIndex === imgIndex) {
           reserveModal.innerHTML = `
           <img src="${theimgs[btnIndex]}" class="dog-image" alt="image of the selected dog"/>
-          <p class="reserve-data">Reservation data</p>
-          <form id="reserveForm" class="reserveForm">
+          <h2 class="reserve-data">Reservations (data)</h2>
+          <div class="reserve-list">use paragraph here</div>
+          <form id="reserve-form" class="reserve-form">
               <input id="username" class="username" value="" placeholder="Your name">
-              <input id="startdate" class="startdate" value="" placeholder="Start date">
-              <input id="enddate" class="enddate" value="" placeholder="End date">
-              <button type="button" id="submit" class="submit" value="submit">Submit</button>
+              <input type="date" id="startdate" class="startdate" value="" placeholder="Start date">
+              <input type="date" id="enddate" class="enddate" value="" placeholder="End date">
+              <button type="button" id="submit" class="submit" value="submit">Reserve</button>
           </form>`
         }
       })
