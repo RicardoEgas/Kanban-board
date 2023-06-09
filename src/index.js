e5004cff6ffe0de1bccb1a1ce83ae2f38e5181ad
ometman <revomet@gmail.com>
2023-06-09 10:20:29 +0000

    Add item Id to index.js

--- src/index.js
+++ src/index.js
@@ -13,12 +13,13 @@ const images = [];
 const getLikes = await fetchLike();
 const dogs = document.getElementById('dogs-list');
 for (let i = 0; i < 6; i += 1) {
+  const itemId = `Dog${i + 1}`;
   const likes = getLikes.filter((like) => like.item_id === i);
   fetchDogs().then((data) => {
     images.push(data);
     dogs.insertAdjacentHTML('beforeend', `<li class="dogs-items">
     <img class="dogs-img" src="${images[i]}"><img>
-    <h2>Dog ${i + 1} <i class="fa fa-heart-o"></i></h2>
+    <h2> ${itemId} <i class="fa fa-heart-o"></i></h2>
     <p id="like">${likes.length > 0 ? likes[0].likes : 0} likes</p>
     <button>Comments</button>
     <button id ="reserve-btn" class="reserve-btn">Reservations</button>
@@ -38,9 +39,9 @@ setTimeout(() => {
 document.body.addEventListener('click', (e) => {
   if (e.target.classList.contains('fa-heart-o')) {
     e.target.style.color = 'red';
-    /* eslint-disable max-len */
-    const id = [].indexOf.call(e.target.parentNode.parentNode.parentNode.childNodes, e.target.parentNode.parentNode);
-    /* eslint-disable max-len */
+
+    const id = [].indexOf.call(e.target.parentNode.parentNode.parentNode.childNodes, 
+      e.target.parentNode.parentNode);
     const numLikes = Number(e.target.parentElement.nextElementSibling.textContent.match(/\d+/)[0]) + 1;
     e.target.parentElement.nextElementSibling.innerHTML = `${numLikes} Likes`;
     postLike(id);
