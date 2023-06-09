58d3a872b70b0e3e70b6f84af612f13b6dac8782
RicardoEgas <rj_egas@hotmail.com>
2023-06-09 22:56:57 +0200

    Fixed API Interaction

--- src/modules/saveComments.js
+++ src/modules/saveComments.js
@@ -1,44 +1,24 @@
 export const apiUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/';
 
-const saveComment = async (appId, itemId, username, comment) => {
-  try {
-    const response = await fetch(`${apiUrl}apps/${appId}/comments/`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ item_id: itemId, username, comment }),
-    });
-    if (!response.ok) {
-      throw new Error('Failed to save a comment');
-    }
-    const { result } = await response.json();
-    return result;
-  } catch (error) {
-    return null;
-  }
-};
-
-// const handleSaveComment = (appId) => {
-//   const commentData = getCommentData();
-//   if (appId && commentData) {
-//     const { itemId, username, comment } = commentData;
-//     saveComment(appId, itemId, username, comment);
-//   }
-// };
-
-const initializeApp = async () => {
-  const response = await fetch(`${apiUrl}apps/`, {
+const saveComment = async (itemId, username, comment) => {
+   await fetch(`${apiUrl}apps/HrIKPRrYjrxS00NlIVCD/comments/`, {
     method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      item_id: itemId,
+      username,
+      comment,
+    }),
   });
-  if (!response.ok) {
-    throw new Error('Failed to initialize the app');
-  }
-  // const data = await response.text();
-  // const appId = data.trim();
-  // handleSaveComment(appId);
 };
 
-initializeApp();
+export const handleSaveComment = async (itemId) => {
+  const response = await fetch(`${apiUrl}apps/HrIKPRrYjrxS00NlIVCD/comments?item_id=${itemId}`);
+  const data = await response.json();
+  return data;
+};
+
 
 export default saveComment;
