3cad67954c486e95fedeae1bd9bf7df4823816ba
RicardoEgas <rj_egas@hotmail.com>
2023-06-11 00:18:08 +0200

    Fixed ESLinter

--- src/tests/commentCount.test.js
+++ src/tests/commentCount.test.js
@@ -1,8 +1,6 @@
-import { apiUrl } from './saveComment.js';
-
 const getCommentCount = async (itemId) => {
   const response = await fetch(
-    `${apiUrl}apps/HrIKPRrYjrxS00NlIVCD/comments?item_id=${itemId}`,
+    `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/HrIKPRrYjrxS00NlIVCD/comments?item_id=${itemId}`,
   );
   const data = await response.json();
   return data.length;
